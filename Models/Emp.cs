1231b5d2b2fa26f61b7c6f4afa7c973df04d6b6f
Afreed15 <Afreed.vs786@gmail.com>
2021-03-16 10:45:21 +0530

    second commit

--- Models/Emp.cs
+++ Models/Emp.cs
@@ -7,5 +7,9 @@ namespace branchingExMVC.Models
 {
     public class Emp
     {
+        public int Empid { get; set; }
+        public string EName { get; set; }
+        public string EDesign { get; set; }
+        public DateTime? EDoj { get; set; }
     }
 }
